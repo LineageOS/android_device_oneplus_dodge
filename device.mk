621d0fc27f72aef7470e50863c38ed9f5f75c8c4
Michael Bestas <mkbestas@lineageos.org>
2025-07-19 10:55:59 +0300

    dodge: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
+# Power
+$(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
+
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare-service.oplus
